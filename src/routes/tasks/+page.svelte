50c11365aa887531a8e36c7bb044f830fb49fbd4
Braeden Smith <braedensmith629@gmail.com>
2023-06-11 17:20:46 -0500

    Add tasks page

---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+  let task = {
+    name: "Example Task",
+    description: "lorem ipsum dolor sit amet",
+    complete: false,
+    dueDate: "2023-6-11",
+    projectId: 1
+  }
+
+</script>
+
+<div class="content">
+  <p>test</p>
+</div>
+
+<style>
+  .content {
+    width: 50%;
+    margin: auto;
+  }
+</style>
