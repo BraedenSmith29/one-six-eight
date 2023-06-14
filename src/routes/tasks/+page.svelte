c8582b4f8d80b49ad62913f80a485cc644722db3
Braeden Smith <braedensmith629@gmail.com>
2023-06-13 21:21:22 -0500

    Display complete tasks under incomplete tasks

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,10 +1,22 @@
 <script>
   import TaskListItem from "$lib/components/tasks/TaskListItem.svelte";
   import TaskStore from "$lib/stores/taskStore.js";
+
+  let completeTasks = [];
+  let incompleteTasks = [];
+
+  TaskStore.subscribe(tasks => {
+    completeTasks = tasks.filter(task => task.complete === true);
+    incompleteTasks = tasks.filter(task => task.complete === false);
+  });
+
 </script>
 
 <div class="content">
-  {#each $TaskStore as task}
+  {#each incompleteTasks as task}
+    <TaskListItem task={task} />
+  {/each}
+  {#each completeTasks as task}
     <TaskListItem task={task} />
   {/each}
 </div>
