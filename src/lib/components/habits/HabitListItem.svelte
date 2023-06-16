7c7df9dcef12e9b95110bd6267a0c0914a0e6409
Braeden Smith <braedensmith629@gmail.com>
2023-06-15 23:11:01 -0500

    Habit completion history now populated on load

--- src/lib/components/habits/HabitListItem.svelte
+++ src/lib/components/habits/HabitListItem.svelte
@@ -1,23 +1,25 @@
 <script>
   import { getLastSevenDays, convertDateFormat } from "$lib/shared/dateHelper.js";
   
-  let historyWindow = getLastSevenDays();
-  
-  let habit = {
+  export let habit = {
     id: 1,
     name: "Habit 1",
     type: "boolean",
     color: "#c6c6c6",
   };
-  let habitHistory = ["2023-6-15", "2023-6-14", "2023-6-12", "2023-6-10"];
+  
+  let habitHistory = ["2023-06-15", "2023-06-14", "2023-06-12", "2023-06-10"];
+  
+  // By default, let history window be the last seven days and pre-load it with completion status based on habit history
+  let historyWindow = getLastSevenDays().map(date => ({ date, completed: habitHistory.includes(date) }));
 </script>
 
 <div class="habit-list-item" style="background-color: {habit.color};">
   <h3>{habit.name}</h3>
   {#each historyWindow as day}
     <div>
-      <div>{convertDateFormat(day)}</div>
-      <input type="checkbox">
+      <div>{convertDateFormat(day.date)}</div>
+      <input type="checkbox" bind:checked={day.completed}>
     </div>
   {/each}
 </div>
