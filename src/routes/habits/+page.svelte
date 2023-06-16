7f91c5f25689a27c764725dc3ea9afc32bd6029d
Braeden Smith <braedensmith629@gmail.com>
2023-06-15 22:06:22 -0500

    Added basic habits page

---
+++ src/routes/habits/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<div class="content">
+  <button class="add-habit-button">Add Habit</button>
+</div>
+
+<style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 10px;
+    width: 50%;
+    margin: auto;
+  }
+  .add-habit-button {
+    width: 20%;
+  }
+</style>
