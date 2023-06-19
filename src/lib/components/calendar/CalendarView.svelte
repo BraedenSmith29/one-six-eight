9bbf5b2f89cc16ab8a60c2e6950d8a5a9d9c1798
Braeden Smith <braedensmith629@gmail.com>
2023-06-19 17:30:58 -0500

    Set up basic skeleton for calendar view

---
+++ src/lib/components/calendar/CalendarView.svelte
@@ -0,0 +1,68 @@
+<script>
+  // Helper functions
+  import { getArrayOfDays, getCurrentDate, parseDateString } from "$lib/shared/dateHelper.js";
+
+  let dateWindow = getArrayOfDays(7, 2).map(d => parseDateString(d));
+</script>
+
+<div class="controls-header">
+  <button>&lt;</button>
+  <h1 style="display: inline;">June 2023</h1>
+  <button>&gt;</button>
+</div>
+<div class="day-header-wrapper">
+  {#each dateWindow as date}
+    <div class="day-header">
+      <div class="date" class:current-date={getCurrentDate() === date.dateString}>
+        <div>{date.dayOfWeek.substring(0, 3)}</div>
+        <div style="font-size: 1.5em">{date.day}</div>
+      </div>
+    </div>
+  {/each}
+</div>
+<div class="contents">
+  {#each dateWindow as date}
+    <div class="day-column">
+      <div style="background-color: azure; height: 200px; width: 90%">
+        Example Cal Event
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .controls-header {
+    display: flex;
+    margin: 10px auto;
+    gap: 10px;
+    justify-content: center;
+  }
+  .controls-header h1 {
+    margin: 0;
+  }
+  .day-header-wrapper {
+    display: flex;
+    border-bottom: 1px solid grey;
+    margin: 10px 0 0;
+    padding-bottom: 20px;
+  }
+  .day-header {
+    flex: 1;
+  }
+  .date {
+    width: fit-content;
+    margin: 0 auto;
+    padding: 10px;
+    text-align: center;
+  }
+  .current-date {
+    background-color: #36b2ff;
+  }
+  .contents {
+    flex: 1;
+    display: flex;
+  }
+  .day-column {
+    flex: 1;
+  }
+</style>
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,15 +1,24 @@
 <script>
   import CalendarSidebar from "$lib/components/calendar/CalendarSidebar.svelte";
+  import CalendarView from "$lib/components/calendar/CalendarView.svelte";
 </script>
 
 <div class="calendar-view-wrapper">
   <CalendarSidebar />
-  <h1>Page Content</h1>
+  <div class="calendar-view">
+    <CalendarView />
+  </div>
 </div>
 
 <style>
   .calendar-view-wrapper {
     display: flex;
     height: 100%;
+  }
+  .calendar-view {
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
   }
 </style>
