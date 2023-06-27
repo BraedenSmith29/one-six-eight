6685cec97cb56e6caa136322d4ef4d8330050746
Braeden Smith <braedensmith629@gmail.com>
2023-06-27 14:36:32 -0500

    Make dummy data current for easier testing

--- src/lib/stores/eventStore.js
+++ src/lib/stores/eventStore.js
@@ -4,16 +4,16 @@ const eventStore = writable([
   {
     id: 1,
     name: "Event 1",
-    startTime: "2023-06-20T08:00",
-    endTime: "2023-06-20T10:00",
+    startTime: "2023-06-28T08:00",
+    endTime: "2023-06-28T10:00",
     description: "lorem ipsum dolor sit amet",
     calendarId: 1
   },
   {
     id: 2,
     name: "Event 2",
-    startTime: "2023-06-21T09:00",
-    endTime: "2023-06-21T13:00",
+    startTime: "2023-06-29T09:00",
+    endTime: "2023-06-29T13:00",
     description: "lorem ipsum dolor sit amet",
     calendarId: 1
   },
