3def1dbc44f591fb63c0576ad9ace80e2f549be4
Braeden Smith <braedensmith629@gmail.com>
2023-07-11 11:07:39 -0500

    Rearranged routing structure

---
+++ src/routes/(default)/+layout.svelte
@@ -0,0 +1,36 @@
+<header>
+  <div class="header-group">
+    <span class="site-title">One Six Eight</span>
+    <a href="/app">Enter App</a>
+  </div>
+  <div class="header-group">
+    <a href="/auth">Auth</a>
+  </div>
+</header>
+<slot />
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f5f5f5;
+    border-bottom: 2px solid grey;
+  }
+  .header-group {
+    display: flex;
+    align-items: center;
+    gap: 25px;
+  }
+  header a {
+    font-size: 1.25em;
+  }
+  .site-title {
+    font-size: 2em;
+    font-weight: bold;
+    border-right: 2px solid grey;
+    padding-left: 10px;
+    padding-right: 25px;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script>
-  // Assets
-  import profilePicture from "$lib/assets/profile-temp.png";
-  // Lifecycle Functions
+  // Helper Functions
   import { invalidate } from '$app/navigation';
+  // Lifecycle Functions
   import { onMount } from 'svelte';
   // Stores
   import { page } from "$app/stores";
@@ -20,46 +19,4 @@
   });
 </script>
 
-<header>
-  <div class="header-group">
-    <span class="site-title">One Six Eight</span>
-    <a href="/calendar">Calendar</a>
-    <a href="/tasks">Tasks</a>
-    <a href="/habits">Habits</a>
-  </div>
-  <div class="header-group">
-    <a href="/insights">Insights</a>
-    <a href="/auth">Auth</a>
-    <img class="profile-picture" alt="Profile" src={profilePicture} />
-  </div>
-</header>
 <slot />
-
-<style>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
-    background-color: #f5f5f5;
-    border-bottom: 2px solid grey;
-  }
-  .header-group {
-    display: flex;
-    align-items: center;
-    gap: 25px;
-  }
-  header a {
-    font-size: 1.25em;
-  }
-  .site-title {
-    font-size: 2em;
-    font-weight: bold;
-    border-right: 2px solid grey;
-    padding-left: 10px;
-    padding-right: 25px;
-  }
-  .profile-picture {
-    height: 50px;
-  }
-</style>
