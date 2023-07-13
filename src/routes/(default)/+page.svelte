2d742698c0ddaa8f0ecfdd1781022eea828e70ba
Braeden Smith <braedensmith629@gmail.com>
2023-07-12 21:27:15 -0500

    Customized landing page with auth options

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Stores
+  import { page } from "$app/stores";
+</script>
+
+<div class="content">
+  <h1>Welcome to One Six Eight</h1>
+  <p>This is a temporary landing page for the website. To enter the web app, please select an option below.</p>
+  <div class="buttons">
+    {#if $page.data.session}
+      <a href="/app">Enter App</a>
+    {:else}
+      <a href="/login">Login</a>
+      <a href="/create-account">Create Account</a>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .content {
+    max-width: 50%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+  .buttons {
+    margin-top: 30px;
+  }
+  a {
+    padding: 10px 20px;
+    font-size: 18px;
+    font-family: sans-serif;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    cursor: pointer;
+    text-decoration: none;
+  }
+</style>
