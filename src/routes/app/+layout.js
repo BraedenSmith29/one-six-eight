45216eeaacdca970898127b7a0b2d63f828672bb
Braeden Smith <braedensmith629@gmail.com>
2023-07-16 18:55:00 -0500

    No longer need to protect route at layout level

--- src/routes/app/+layout.js
+++ src/routes/app/+layout.js
@@ -1,9 +1,5 @@
-import { redirect } from "@sveltejs/kit";
+export const load = async ({ data }) => {
 
-export const load = async ({ parent }) => {
-  // Need to have a layout-level protection to avoid client-side switching
-  const data = await parent();
-  if (!data.session) {
-    throw redirect(303, "/");
-  }
+  console.log(data);
+
 }
