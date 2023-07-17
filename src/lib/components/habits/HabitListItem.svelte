1b9770668b596e01374be753f36fa1bb5f67c13e
Braeden Smith <braedensmith629@gmail.com>
2023-07-17 18:26:54 -0500

    Can update habit history

--- src/lib/components/habits/HabitListItem.svelte
+++ src/lib/components/habits/HabitListItem.svelte
@@ -1,27 +1,50 @@
 <script>
   // Helper functions
   import { getLastSevenDays, convertDateFormat } from "$lib/shared/dateHelper.js";
-  
   // Stores
+  import { page } from "$app/stores";
   import habitStore from "$lib/stores/habitStore.js";
-  
   // Properties
   export let habit;
   
   // Reactively define historyWindow as an array of objects with the date and completion status for that date
   $: historyWindow = getLastSevenDays().map(date => ({ date, completed: habit.history.includes(date) }));
 
-  const updateHistory = (day) => {
+  let habitHistoryLoading = []; // Array of dates that are loading
+  const updateHistory = async (day) => {
+    if (habitHistoryLoading.includes(day.date)) return;
+    habitHistoryLoading = [...habitHistoryLoading, day.date];
+
     // Logic is sorta backwards
     if (!day.completed) {
       // If the day currently IS NOT completed, then it needs to be ADDED to the list
-      habit.history = [...habit.history, day.date];
+      const { error } = await $page.data.supabase
+        .from("habit_history")
+        .insert({
+          habit_id: habit.id,
+          entry_date: day.date,
+        });
+      if (!error) {
+        habit.history = [...habit.history, day.date];
+      } else {
+        console.log("There was an error completing " + day.date + ": " + error);
+      }
     } else {
       // If the day currently IS completed, then it needs to be REMOVED from the list
-      habit.history = habit.history.filter(d => d !== day.date);
+      const { error } = await $page.data.supabase
+        .from("habit_history")
+        .delete()
+        .match({ habit_id: habit.id, entry_date: day.date });
+      if (!error) {
+        habit.history = habit.history.filter(d => d !== day.date);
+      } else {
+        console.log("There was an error uncompleting " + day.date + ": " + error);
+      }
     }
     // Push the update to the store
     $habitStore = $habitStore;
+
+    habitHistoryLoading.filter(d => d !== day.date);
   };
 </script>
 
