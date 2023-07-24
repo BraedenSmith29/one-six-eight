64df9f01dfd8617c993de8f9032f497436922618
Braeden Smith <braedensmith629@gmail.com>
2023-07-23 20:51:52 -0500

    Extracted add group to special button component

---
+++ src/lib/components/shared/AddGroupButton.svelte
@@ -0,0 +1,99 @@
+<script>
+  // Assets
+  import addIcon from "$lib/assets/add-icon.svg";
+  // Components
+  import Modal from "$lib/components/shared/Modal.svelte";
+  // Stores
+  import { page } from "$app/stores";
+
+  // Add functionality and form contents variable for modal
+  let addGroupFields;
+  let showModal = false;
+  const toggleAddGroupModal = () => {
+    // Clear the fields and toggle the modal
+    addGroupFields = {
+      title: "",
+      color: "#c6c6c6",
+    };
+    showModal = !showModal
+  };
+
+  let loading = false;
+  let addGroupError = null;
+  const addGroup = async () => {
+    if (loading) return;
+    loading = true;
+
+    const { data, error } = await $page.data.supabase
+      .from("groups")
+      .insert({
+        title: addGroupFields.title,
+        color: addGroupFields.color.substring(1),
+      })
+      .select().single();
+
+    addGroupError = error;
+    if (!error) {
+      groupStore.update(groups => {
+        let newGroup = {
+          id: data.id,
+          title: addGroupFields.title,
+          color: addGroupFields.color,
+          show: true,
+        }
+        return [...groups, newGroup];
+      });
+      toggleAddGroupModal();
+    }
+    
+    loading = false;
+  };
+</script>
+
+<button class="add-new-group-button" on:click={toggleAddGroupModal}>
+  <img src={addIcon} />
+  <span>Add New Group</span>
+</button>
+<Modal showModal={showModal} on:exit={toggleAddGroupModal}>
+  <form class="add-group-modal" on:submit|preventDefault={addGroup}>
+    <label>
+      <span>Group Name: </span>
+      <input type="textbox" bind:value={addGroupFields.title}>
+    </label>
+    <label>
+      <span>Habit Color: </span>
+      <input type="color" bind:value={addGroupFields.color}>
+    </label>
+    {#if loading}
+      <p>Loading...</p>
+    {:else}
+      <button type="submit">Add Group</button>
+    {/if}
+    {#if addGroupError}
+      <p class="error-text">{addGroupError}</p>
+    {/if}
+  </form>
+</Modal>
+
+<style>
+  .add-new-group-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    background-color: #353535;
+    color: inherit;
+    font-size: 1em;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  .add-group-modal label {
+    display: block;
+    margin: 5px 0px;
+  }
+  .error-text {
+    color: red;
+  }
+</style>
--- src/routes/app/tasks/Sidebar.svelte
+++ src/routes/app/tasks/Sidebar.svelte
@@ -6,55 +6,10 @@
   import inboxIcon from "$lib/assets/inbox-icon.svg";
   import selectedGroupIcon from "$lib/assets/selected-group-icon.svg";
   import unselectedGroupIcon from "$lib/assets/unselected-group-icon.svg";
-  import addIcon from "$lib/assets/add-icon.svg";
   // Components
-  import Modal from "$lib/components/shared/Modal.svelte";
+  import AddGroupButton from "$lib/components/shared/AddGroupButton.svelte";
   // Stores
-  import { page } from "$app/stores";
   import groupStore from "$lib/stores/groupStore.js";
-
-  // Add functionality and form contents variable for modal
-  let addGroupFields;
-  let showModal = false;
-  const toggleAddGroupModal = () => {
-    // Clear the fields and toggle the modal
-    addGroupFields = {
-      title: "",
-      color: "#c6c6c6",
-    };
-    showModal = !showModal
-  };
-
-  let loading = false;
-  let addGroupError = null;
-  const addGroup = async () => {
-    if (loading) return;
-    loading = true;
-
-    const { data, error } = await $page.data.supabase
-      .from("groups")
-      .insert({
-        title: addGroupFields.title,
-        color: addGroupFields.color.substring(1),
-      })
-      .select().single();
-
-    addGroupError = error;
-    if (!error) {
-      groupStore.update(groups => {
-        let newGroup = {
-          id: data.id,
-          title: addGroupFields.title,
-          color: addGroupFields.color,
-          show: true,
-        }
-        return [...groups, newGroup];
-      });
-      toggleAddGroupModal();
-    }
-    
-    loading = false;
-  };
 </script>
 
 <div class="sidebar">
@@ -87,31 +42,8 @@
       {/each}
     {/if}
   </div>
-  <button class="add-new-group-button" on:click={toggleAddGroupModal}>
-    <img src={addIcon} />
-    <span>Add New Group</span>
-  </button>
+  <AddGroupButton />
 </div>
-<Modal showModal={showModal} on:exit={toggleAddGroupModal}>
-  <form class="add-group-modal" on:submit|preventDefault={addGroup}>
-    <label>
-      <span>Group Name: </span>
-      <input type="textbox" bind:value={addGroupFields.title}>
-    </label>
-    <label>
-      <span>Habit Color: </span>
-      <input type="color" bind:value={addGroupFields.color}>
-    </label>
-    {#if loading}
-      <p>Loading...</p>
-    {:else}
-      <button type="submit">Add Group</button>
-    {/if}
-    {#if addGroupError}
-      <p class="error-text">{addGroupError}</p>
-    {/if}
-  </form>
-</Modal>
 
 <style>
   .sidebar {
@@ -160,25 +92,5 @@
   }
   .selected {
     background-color: #353535;
-  }
-  .add-new-group-button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 5px;
-    background-color: #353535;
-    color: inherit;
-    font-size: 1em;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  .add-group-modal label {
-    display: block;
-    margin: 5px 0px;
-  }
-  .error-text {
-    color: red;
   }
 </style>
