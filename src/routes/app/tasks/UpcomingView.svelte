6425fb199bd350458cf5faeb5cfe7c411b19d71d
Braeden Smith <braedensmith629@gmail.com>
2023-07-25 21:33:07 -0500

    Very basic header framework for upcoming view

--- src/routes/app/tasks/UpcomingView.svelte
+++ src/routes/app/tasks/UpcomingView.svelte
@@ -1,1 +1,26 @@
-<p>Placeholder teehee</p>
+<div class="content">
+  <div style="position: relative;" class="list-header">
+    <h2>June 2023 v</h2>
+    <spacer style="flex-grow: 1" />
+    <p>Filters</p>
+    <button>Today</button>
+  </div>
+  <hr style="width: 100%"/>
+</div>
+
+
+<style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 10px;
+    width: 50%;
+    margin: 10px auto;
+    color: #eeeeee;
+  }
+  .list-header {
+    display: flex;
+    align-items: center;
+  }
+</style>
